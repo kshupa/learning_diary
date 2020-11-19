7bb4735bb45cbdb63bcfa78cbaf98fcc7f3ac132
christina <christina@christinas-iMac.local>
2020-11-19 16:42:12 -0600

    Defining the entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
@@ -10,3 +11,18 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of a model."""
         return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
+    
