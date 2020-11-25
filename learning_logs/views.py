b42f2e0d8fd232579cd41b9f72abf6fbb4148fab
christina <christina@christinas-iMac.local>
2020-11-24 19:03:03 -0600

    topics view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """The home page for learning diary."""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """The page that shows all topics."""
+    topics = Topic.object.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html')
