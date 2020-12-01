4d7f7d28765d0b5dad6e5619bf4eac15537ba4f8
christina <christina@christinas-iMac.local>
2020-12-01 13:48:36 -0600

    added new_topic views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
+
 
 def index(request):
     """The home page for learning diary."""
@@ -19,3 +21,20 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; prosses data.
+        form = TopicForm(data=request.POST)
+        if form is_valid():
+            form.save()
+            return redirect('learnind_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
